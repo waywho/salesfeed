9890a61168dcd8b3240b822586c96dd2c01af052
waywho <walzerfan@yahoo.com>
2017-04-03 19:44:44 +0000

    seed logo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -27,7 +27,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],                         # required
+    :region => ENV['AWS_REGION'] # Required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
