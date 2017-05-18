af0b176869d154165e965df9b8e2163aa6d2bf2c
waywho <walzerfan@yahoo.com>
2017-05-18 08:18:16 +0000

    fix bug and add copy style

--- app/assets/javascripts/master.js
+++ app/assets/javascripts/master.js
@@ -1,50 +1,50 @@
 $(document).ready(function() {
-      var modalSelector = '[data-modal]';
-      var openModalSelector = '[data-open-modal]';
-      var closeModalSelector = '[data-close-modal]';
+    var modalSelector = '[data-modal]';
+    var openModalSelector = '[data-open-modal]';
+    var closeModalSelector = '[data-close-modal]';
 
-      $(openModalSelector).on('click', function(event) {
-        var openButton = $(event.target);
-        var modal = modalForOpenButton(openButton);
-        var closeButton = closeButtonForModal(modal);
-        modal.attr('aria-hidden', false);
-        closeButton.focus();
-      });
+    $(openModalSelector).on('click', function(event) {
+      var openButton = $(event.target);
+      var modal = modalForOpenButton(openButton);
+      var closeButton = closeButtonForModal(modal);
+      modal.attr('aria-hidden', false);
+      closeButton.focus();
+    });
 
-      function closeModal(modal) {
-        var openButton = openButtonForModal(modal);
-        modal.attr('aria-hidden', true);
-        openButton.focus();
-      }
+    function closeModal(modal) {
+      var openButton = openButtonForModal(modal);
+      modal.attr('aria-hidden', true);
+      openButton.focus();
+    }
 
-      $(closeModalSelector).on('click', function(event) {
-        var closeButton = $(event.target);
-        var modal = modalForCloseButton(closeButton);
+    $(closeModalSelector).on('click', function(event) {
+      var closeButton = $(event.target);
+      var modal = modalForCloseButton(closeButton);
+      closeModal(modal);
+    });
+
+    $(document).on('keyup', function(event) {
+      var ESCAPE_KEY_CODE = 27;
+
+      if(event.keyCode === ESCAPE_KEY_CODE) {
+        var modal = $(modalSelector + '[aria-hidden=false]');
         closeModal(modal);
-      });
-
-      $(document).on('keyup', function(event) {
-        var ESCAPE_KEY_CODE = 27;
-
-        if(event.keyCode === ESCAPE_KEY_CODE) {
-          var modal = $(modalSelector + '[aria-hidden=false]');
-          closeModal(modal);
-        }
-      });
-
-      function openButtonForModal(modal) {
-        return $('[data-open-modal=' + modal.data('modal') + ']');
       }
+    });
 
-      function closeButtonForModal(modal) {
-        return modal.find($(closeModalSelector));
-      }
+    function openButtonForModal(modal) {
+      return $('[data-open-modal=' + modal.data('modal') + ']');
+    }
 
-      function modalForOpenButton(openButton) {
-        return $('[data-modal=' + openButton.data('open-modal') + ']');
-      }
+    function closeButtonForModal(modal) {
+      return modal.find($(closeModalSelector));
+    }
 
-      function modalForCloseButton(closeButton) {
-        return closeButton.closest($(modalSelector));
-      }
+    function modalForOpenButton(openButton) {
+      return $('[data-modal=' + openButton.data('open-modal') + ']');
+    }
+
+    function modalForCloseButton(closeButton) {
+      return closeButton.closest($(modalSelector));
+    }
 });
