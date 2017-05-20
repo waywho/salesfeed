73f8be5fd001d20fa458060554cf546682c07e91
waywho <walzerfan@yahoo.com>
2017-05-20 14:07:27 +0000

    add post script

--- app/assets/javascripts/master.js
+++ app/assets/javascripts/master.js
@@ -47,4 +47,19 @@ $(document).ready(function() {
     function modalForCloseButton(closeButton) {
       return closeButton.closest($(modalSelector));
     }
+
+    $(".more-click").click(function() {
+      var link = $(this);
+      var message = $(this).siblings(".message")
+      $(message).toggle("fast", function() {
+        if ($(this).is(':visible')) {
+          link.text('Show Less');
+        } else {
+          link.text('Show More');
+        }
+      });
+    });
+    $('.deal_image').error(function() {
+      $(this).attr('src', '/assets/deal_image_holder.png');
+    });
 });
